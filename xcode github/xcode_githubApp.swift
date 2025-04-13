8e1c9ea0ce797d104b62ea8e9de00a8dd26f1878
jiwonpark831 <jiwonpark831@gmail.com>
2025-04-13 21:18:16 +0900

    Initial Commit

---
+++ xcode github/xcode_githubApp.swift
@@ -0,0 +1,17 @@
+//
+//  xcode_githubApp.swift
+//  xcode github
+//
+//  Created by jiwon on 4/13/25.
+//
+
+import SwiftUI
+
+@main
+struct xcode_githubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ xcode githubTests/xcode_githubTests.swift
@@ -0,0 +1,16 @@
+//
+//  xcode_githubTests.swift
+//  xcode githubTests
+//
+//  Created by jiwon on 4/13/25.
+//
+
+import Testing
+
+struct xcode_githubTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
